b75ca91c31017c93b53f7336340793f63bd33db9
HighnessAtharva <HighnessAtharva@gmail.com>
2023-04-06 22:07:59 +0530

    place_order and checkout_html added

--- FoodOnline_Main/urls.py
+++ FoodOnline_Main/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path("cart/", marketplace_views.cart, name="cart"),
     path("search/", marketplace_views.search, name="search"),
     path("checkout/", marketplace_views.checkout, name="checkout"),
+    path("orders/", include("orders.urls")),    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from marketplace.models import Cart 
+from marketplace.context_processors import get_cart_amount
+from .forms import OrderForm
+from .models import Order
+import simplejson as json
 
 # Create your views here.
+def place_order(request):
+    cart_items = Cart.objects.filter(user=request.user).order_by("created_at")
+    cart_count = cart_items.count()
+    if cart_count < 0:
+         return redirect('marketplace')
+     
+    subtotal= get_cart_amount(request)['subtotal']
+    total_tax= get_cart_amount(request)['tax'] 
+    grand_total= get_cart_amount(request)['grand_total'] 
+    tax_data= get_cart_amount(request)['tax_dict']
+    
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            order=Order()
+            order.first_name = form.cleaned_data['first_name']
+            order.last_name = form.cleaned_data['last_name']
+            order.phone = form.cleaned_data['phone']
+            order.email = form.cleaned_data['email']
+            order.address = form.cleaned_data['address']
+            order.country = form.cleaned_data['country']
+            order.state = form.cleaned_data['state']
+            order.city = form.cleaned_data['city']
+            order.pin_code = form.cleaned_data['pin_code']
+
+            order.user = request.user
+            order.total = grand_total
+            order.tax_data = json.dumps(tax_data)
+            order.total_tax = total_tax
+            order.payment_method = request.POST['payment_method']
+            order.order_number = '123'
+            order.save()
+            
+            context= {
+                'order': order,
+                'cart_items': cart_items,
+            }
+            
+            return render(request, 'orders/place_order.html', context)
+        else:
+            print(form.errors)
+    return render(request, 'orders/place_order.html')
